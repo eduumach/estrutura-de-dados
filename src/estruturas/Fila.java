648d5fb79acd4d2022776f3eaafe202bcff1bbda
Eduardo Machado Rezende <eduardomachadorezende0@gmail.com>
2023-04-14 22:09:24 -0300

    Adciona estrtura de Fila

--- src/estruturas/Fila.java
+++ src/estruturas/Fila.java
@@ -1,12 +1,6 @@
 package estruturas;
 
 public class Fila {
-//    Enfileirar
-//    desenfileirar
-//    esta vazia
-//    esta cheia
-//    fim da fila
-
     private int tamanhoMax;
     private int[] filaArray;
     private int inicio;
@@ -21,8 +15,62 @@ public class Fila {
         this.tamanho = 0;
     }
 
+    public boolean isEmpty(){
+        return (tamanho == 0);
+    }
+
+    public boolean isFull(){
+        return (tamanho == tamanhoMax);
+    }
+
+    public int peek(){
+        if(isEmpty() ){
+            System.out.println("A Fila está vazia!");
+            return -1;
+        }else {
+            return filaArray[inicio];
+        }
+    }
+
+    public void enqueue(int elemento){
+        if (this.isFull()){
+            System.out.println("Fila esta cheia");
+        } else {
+            if(fim == tamanhoMax -1){
+                fim = -1;
+            }
+            fim++;
+            filaArray[fim] = elemento;
+            tamanho++;
+        }
+    }
+
+    public int daqueue(){
+        if (this.isEmpty()){
+            System.out.println("A fila esta vazia!");
+            return -1;
+        } else {
+            int elemento = filaArray[inicio];
+            inicio++;
+            if(inicio == tamanhoMax){
+                inicio = 0;
+            }
+            tamanho--;
+            return elemento;
+        }
+    }
+
     public static void main(String[] args) {
         Fila filinha = new Fila(5);
+        filinha.enqueue(21);
+        filinha.enqueue(2);
+        filinha.enqueue(251);
+        filinha.enqueue(243);
+
+        System.out.println(filinha.daqueue());
+        System.out.println(filinha.daqueue());
+        System.out.println(filinha.daqueue());
+        System.out.println(filinha.daqueue());
 
     }
 }
