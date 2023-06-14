ec485f83735cea7b01defb6139424335bb987beb
Eduado Machado Rezende <eduardomachadorezende0@gmail.com>
2023-06-13 22:22:40 -0300

    Tabela Hash

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import estruturas.TableHash;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TableHash tableHash = new TableHash(10);
+        tableHash.put("A");
+        tableHash.put("B");
+        tableHash.put("C");
+        tableHash.put("D");
+        System.out.println(tableHash.busca("A"));
     }
 }
